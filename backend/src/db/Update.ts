bf06dfada5d7ea63a8b68568672dd2523a77ac42
Yushan Fernando <yushanwebdev@gmail.com>
2023-09-10 17:44:58 +0530

    main: Optimise the get all updates query

--- backend/src/db/Update.ts
+++ backend/src/db/Update.ts
@@ -1,6 +1,8 @@
 import { pool } from "./";
 
 export const getAllUpdates = async () => {
-  const allUpdates = await pool.query("SELECT * FROM update");
+  const allUpdates = await pool.query(
+    "SELECT id, title, updated_at FROM update"
+  );
   return allUpdates.rows;
 };
