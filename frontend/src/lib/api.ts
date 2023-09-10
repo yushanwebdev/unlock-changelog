0a3680c0f42156eedd1d3a12c09b65754f2851c7
Yushan Fernando <yushanwebdev@gmail.com>
2023-09-11 01:00:39 +0530

    main: Fix the frontend workflow execution directory issue

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 import { Update } from "./types";
 
 export async function getAllUpdates(): Promise<Update[]> {
-  const res = await fetch("https://unlock-changelog.fly.dev/api/v1/updates");
+  const res = await fetch(`${process.env.PUBLIC_API}/updates`);
 
   if (!res.ok) {
     throw new Error("Failed to fetch all the updates");
@@ -11,9 +11,7 @@ export async function getAllUpdates(): Promise<Update[]> {
 }
 
 export async function getUpdateById(id: number): Promise<Update> {
-  const res = await fetch(
-    `https://unlock-changelog.fly.dev/api/v1/updates/${id}`
-  );
+  const res = await fetch(`${process.env.PUBLIC_API}/updates/${id}`);
 
   if (!res.ok) {
     throw new Error("Failed to fetch the update");
