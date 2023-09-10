66eca0c7d981573decbf449609fa44016280828a
Yushan Fernando <yushanwebdev@gmail.com>
2023-09-10 17:49:56 +0530

    main: Update the get all endpoint name in the backend

--- backend/src/v1/router/update.ts
+++ backend/src/v1/router/update.ts
@@ -2,5 +2,5 @@ import express from "express";
 import { getAllUpdates } from "../../controllers/update";
 
 export default (router: express.Router) => {
-  router.get("/update", getAllUpdates);
+  router.get("/updates", getAllUpdates);
 };
